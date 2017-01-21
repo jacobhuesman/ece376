2d449b0521e2e00ee1f506a455d785c5f3d7710a
jacobhuesman <jakehuesman@gmail.com>
2017-01-21 17:58:03 -0600

    Created script for generating hex file

--- hw02.X/p1.asm
+++ hw02.X/p1.asm
@@ -1,23 +1,36 @@
+; --- Configuration
 #include <p18f4620.inc>
     org 0x800
-    
-; -- Variables --   
 ; unsigned char A, B, C
-A0 equ 0
-B0 equ 1
-C0 equ 2
+#define A0 PORTB
+#define B0 PORTC
+#define C0 PORTD
 
-; A = 5; 
- movlw 5
- movwf A0
+    
+; --- Main Routine ---
+Main:
+  call Init
+  call Run
 
-; B = 6; 
- movlw 6
- movwf B0
+; --- Subroutines ---
+Init:
+  clrf TRISB
+  clrf TRISC
+  clrf TRISD
 
-; C = A + B; 
- movf B0,W
- addwf A0,W
- movwf C0
+Run:
+  ; A = 5; 
+  movlw 5
+  movwf A0
+
+  ; B = 6; 
+  movlw 6
+  movwf B0
+
+  ; C = A + B; 
+  movf B0,W
+  addwf A0,W
+  movwf C0
+  goto Run
  
  END
